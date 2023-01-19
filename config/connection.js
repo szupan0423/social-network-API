e3aa7c0934365dfc7f8a6d80e80871e839f11dd4
szupan0423 <111537299+szupan0423@users.noreply.github.com>
2023-01-18 20:03:52 -0600

    forgot to commit work

---
+++ config/connection.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/socialmedia",
+  {
+    //'mongodb://localhost:27017/socialmedia', {
+
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+module.exports = mongoose.connection;
